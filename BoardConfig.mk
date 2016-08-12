90906ae578ceedff91bbd746a82fa684642ce305
Jason Riordan <jriordan001@gmail.com>
2016-08-12 00:10:38 -0400

    Z00A: assert on M based Ifwi version
    Change-Id: I93e83d275333df0c0cc765236b94d3da45a1ac56

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,8 +17,6 @@
 # inherit from common mofd-common
 -include device/asus/mofd-common/BoardConfigCommon.mk
 
-TARGET_RELEASETOOLS_EXTENSIONS := device/asus/Z00A
-
 # Charger
 BOARD_HEALTHD_CUSTOM_CHARGER_RES := device/asus/Z00A/charger/images
 
@@ -28,6 +26,9 @@ BOARD_RECOVERY_KEY := device/asus/Z00A/keys/recovery_z00a.sig
 
 # Partitions
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 59261286400
+
+# Require Ifwi version
+TARGET_BOARD_INFO_FILE ?= device/asus/Z00A/board-info.txt
 
 # Use the non-open-source parts, if they're present
 -include vendor/asus/Z00A/BoardConfigVendor.mk
